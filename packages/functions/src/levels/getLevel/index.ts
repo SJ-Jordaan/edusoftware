7027f2e3def43135b7572526521eb6f14ef41639
Steven Jordaan <steven.j.jordaan2000@gmail.com>
2024-02-16 14:00:54 +0200

    Finalise basic error handling and get all levels

---
+++ packages/functions/src/levels/getLevel/index.ts
@@ -0,0 +1,40 @@
+import { Level, connectToDatabase } from '@edusoftware/core/databases';
+import { handler } from '@edusoftware/core/handlers';
+import { LambdaResponse, PopulatedLevel } from '@edusoftware/core/types';
+
+/**
+ * Handler for fetching all levels from the database and returning them.
+ * This function connects to the database, queries all level documents, and
+ * populates their associated question IDs. If no levels are found, it returns
+ * an empty array. Errors during database operations are caught and logged,
+ * and may be re-thrown to be handled by the base handler, depending on the
+ * application's error handling strategy.
+ *
+ * @returns {Promise<LambdaResponse<PopulatedLevel[]>>} A promise that resolves
+ * to a LambdaResponse containing an array of populated levels. If no levels
+ * are found, the array will be empty. In the case of an error, it re-throws
+ * the error to be handled by the base error handling mechanism.
+ */
+export const main = handler<PopulatedLevel[]>(
+  async (): Promise<LambdaResponse<PopulatedLevel[]>> => {
+    await connectToDatabase();
+
+    try {
+      const levels: PopulatedLevel[] = await Level.find({}).populate(
+        'questionIds',
+      );
+
+      // Handle "no levels found" as an informational scenario
+      return {
+        statusCode: 200,
+        body: levels, // Could be an empty array if no levels are found
+      };
+    } catch (error) {
+      // Log the error for debugging purposes
+      console.error('Error fetching levels:', error);
+
+      // Re-throw the error for global error handling
+      throw error;
+    }
+  },
+);
