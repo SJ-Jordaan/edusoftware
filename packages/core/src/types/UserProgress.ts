f583f0483588a368bbfac50bca7664958a71a85d
Steven Jordaan <steven.j.jordaan2000@gmail.com>
2024-02-18 15:21:10 +0200

    Add interface for progress document

--- packages/core/src/types/UserProgress.ts
+++ packages/core/src/types/UserProgress.ts
@@ -1,3 +1,5 @@
+import mongoose from 'mongoose';
+
 export interface UserProgress {
   _id: string;
   userId: string;
@@ -16,4 +18,22 @@ export interface UserProgress {
   updatedAt: string; // Date string in ISO format
   __v: number;
   completedAt: string | null; // Date string in ISO format or null
+}
+
+interface IQuestionAttempted {
+  questionId?: mongoose.Types.ObjectId | null;
+  attempts: number;
+  correct: boolean;
+  timeSpent: number; // in seconds
+  hintsUsed: number;
+  scoreEarned: number;
+}
+
+export interface IProgressDocument extends mongoose.Document {
+  userId: string;
+  levelId: mongoose.Types.ObjectId;
+  startedAt?: Date | null;
+  completedAt?: Date | null;
+  totalScore: number;
+  questionsAttempted: IQuestionAttempted[];
 }
