99297fa66560143f156d70531e105fffa0999771
LucindaBotes <lucinda.b2000@gmail.com>
2024-02-18 15:48:41 +0200

    #4: Migrate start level to lambda function

---
+++ packages/functions/src/progress/startLevel/index.ts
@@ -0,0 +1,82 @@
+import {
+  Level,
+  Progress,
+  connectToDatabase,
+} from '@edusoftware/core/databases';
+import { handler } from '@edusoftware/core/handlers';
+import {
+  ApplicationError,
+  BadRequestError,
+  LambdaResponse,
+  NotFoundError,
+  UserProgress,
+} from '@edusoftware/core/types';
+import { APIGatewayProxyEventV2 } from 'aws-lambda';
+import { useSessionWithRoles } from '@edusoftware/core/handlers';
+
+/**
+ * Lambda function to retrieve a specific level by its ID, including all related question IDs.
+ * It validates the request, fetches the level, and handles errors appropriately.
+ *
+ * @param {APIGatewayProxyEventV2} event - The event object from AWS Lambda, containing the request parameters.
+ * @returns {Promise<LambdaResponse<UserProgress>>} A promise that resolves to the fetched level data.
+ * @throws {BadRequestError} Thrown if the level ID is not provided in the request.
+ * @throws {NotFoundError} Thrown if no level is found matching the provided ID.
+ */
+export const main = handler<UserProgress>(
+  async (
+    event: APIGatewayProxyEventV2,
+  ): Promise<LambdaResponse<UserProgress>> => {
+    await connectToDatabase();
+    const levelId = event.pathParameters?.id;
+    const { userId } = await useSessionWithRoles();
+
+    if (!levelId) {
+      throw new BadRequestError('Level ID must be provided');
+    }
+
+    try {
+      const levelDoc = await Level.findById(levelId);
+
+      if (!levelDoc) {
+        throw new NotFoundError(`Level with ID ${levelId}`);
+      }
+
+      let progress = await Progress.findOne({ userId, levelId });
+
+      if (progress) {
+        progress.questionsAttempted.pull({});
+        progress.startedAt = new Date();
+        progress.completedAt = null;
+        progress.totalScore = 0;
+      } else {
+        progress = new Progress({ userId, levelId });
+      }
+
+      await progress.save();
+
+      return {
+        statusCode: 200,
+        body: progress.toObject() as UserProgress,
+      };
+    } catch (error: unknown) {
+      if (error instanceof ApplicationError) {
+        throw error;
+      }
+
+      if (error instanceof Error) {
+        console.error(`Failed to get level: ${error.message}`);
+        throw new ApplicationError(
+          `Failed to get level: ${error.message}`,
+          500,
+        );
+      }
+
+      console.error(`Failed to get level: ${error}`);
+      throw new ApplicationError(
+        'Failed to get level due to unexpected error',
+        500,
+      );
+    }
+  },
+);
