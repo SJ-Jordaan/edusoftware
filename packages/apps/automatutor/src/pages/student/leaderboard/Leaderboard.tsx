e0151d4ac6f7255b90323906be870ffcce8f9a4b
Steven Jordaan <steven.j.jordaan2000@gmail.com>
2024-03-15 18:45:31 +0200

    Clean up leaderboard style with student number

--- packages/apps/automatutor/src/pages/student/leaderboard/Leaderboard.tsx
+++ packages/apps/automatutor/src/pages/student/leaderboard/Leaderboard.tsx
@@ -24,36 +24,42 @@ export const LeaderBoard = () => {
           <div className="absolute -top-8 h-16 w-16">
             <Second className="h-full w-full" />
           </div>
-          <p className="line-clamp-2 max-w-full overflow-hidden text-ellipsis text-sm text-white">
-            {second?.userDetails?.name ?? 'None'}
-          </p>
-          <p className="line-clamp-1 max-w-full overflow-hidden text-ellipsis text-xs text-gray-400">
-            {extractStudentNumber(first?.userDetails?.email) ?? 'None'}
-          </p>
+          <div>
+            <p className="line-clamp-2 max-w-full overflow-hidden text-ellipsis text-sm text-white">
+              {second?.userDetails?.name ?? 'None'}
+            </p>
+            <p className="line-clamp-1 max-w-full overflow-hidden text-ellipsis text-xs text-gray-400">
+              {extractStudentNumber(first?.userDetails?.email) ?? 'None'}
+            </p>
+          </div>
           <p className="text-xl text-blue-400">{second?.totalScore ?? 0}</p>
         </div>
         <div className="relative h-44 space-y-2 rounded-t-xl bg-neutral-600 p-4 pt-6 dark:bg-slate-600">
           <div className="absolute -top-8 h-16 w-16">
             <First className="h-full w-full" />
           </div>
-          <p className="line-clamp-2 max-w-full overflow-hidden text-ellipsis text-sm text-white">
-            {first?.userDetails?.name ?? 'None'}
-          </p>
-          <p className="line-clamp-1 max-w-full overflow-hidden text-ellipsis text-xs text-gray-400">
-            {extractStudentNumber(first?.userDetails?.email) ?? 'None'}
-          </p>
+          <div>
+            <p className="line-clamp-2 max-w-full overflow-hidden text-ellipsis text-sm text-white">
+              {first?.userDetails?.name ?? 'None'}
+            </p>
+            <p className="line-clamp-1 max-w-full overflow-hidden text-ellipsis text-xs text-gray-400">
+              {extractStudentNumber(first?.userDetails?.email) ?? 'None'}
+            </p>
+          </div>
           <p className="text-xl text-orange-400">{first?.totalScore ?? 0}</p>
         </div>
         <div className="relative h-36 space-y-2 self-end rounded-r-xl bg-neutral-600 p-4 pt-6 dark:bg-slate-700">
           <div className="absolute -top-8 h-16 w-16">
             <Third className="h-full w-full" />
           </div>
-          <p className="line-clamp-2 max-w-full overflow-hidden text-ellipsis text-sm text-white">
-            {third?.userDetails?.name ?? 'None'}
-          </p>
-          <p className="line-clamp-1 max-w-full overflow-hidden text-ellipsis text-xs text-gray-400">
-            {extractStudentNumber(first?.userDetails?.email) ?? 'None'}
-          </p>
+          <div>
+            <p className="line-clamp-2 max-w-full overflow-hidden text-ellipsis text-sm text-white">
+              {third?.userDetails?.name ?? 'None'}
+            </p>
+            <p className="line-clamp-1 max-w-full overflow-hidden text-ellipsis text-xs text-gray-400">
+              {extractStudentNumber(first?.userDetails?.email) ?? 'None'}
+            </p>
+          </div>
           <p className="text-xl text-green-400">{third?.totalScore ?? 0}</p>
         </div>
       </div>
